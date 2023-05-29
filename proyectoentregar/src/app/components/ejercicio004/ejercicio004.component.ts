ed86a7e28ef8320120118c6fd71fcf289143cc52
JuanluIL <125955129+JuanluIL@users.noreply.github.com>
2023-05-29 14:11:45 +0200

    proyectoAngular

--- proyectoentregar/src/app/components/ejercicio004/ejercicio004.component.ts
+++ proyectoentregar/src/app/components/ejercicio004/ejercicio004.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Fruta } from 'src/app/interfaces/fruta';
+import { Fruta } from 'src/app/interfaces/Fruta';
+
+
 
 import { Ejercicio004Service } from 'src/app/services/ejercicio004.service';
 
@@ -29,11 +31,39 @@ export class Ejercicio004Component implements OnInit{
     this.ejercicio004Service.recuperarFrutas().subscribe(result => this.frutas = result);
   }
 
-  baja(F:Fruta){
-
+  eliminar(id: number){
+    this.ejercicio004Service.eliminar(id).subscribe((datos:Fruta) => {
+      if(datos){
+        alert("Registro eliminado");
+        this.recuperarFrutas();
+      }
+    })
   }
 
   seleccionar(f: Fruta){
+    this.fruta.descripcion = f.descripcion;
+    this.fruta.precio = f.precio;
+    this.fruta.id = f.id;
+  }
 
+  insertar(){
+    this.ejercicio004Service.insertar(this.fruta).subscribe((datos:any) => {
+      if(datos){
+        alert("Registro insertado");
+        this.recuperarFrutas();
+      }
+    })
+  }
+
+  modificar(id: number){
+    this.ejercicio004Service.modificar(id, this.fruta).subscribe((datos:Fruta) => {
+      if(datos){
+        alert("Registro modificado");
+        this.recuperarFrutas();
+      }
+    })
+    this.fruta.descripcion = "";
+    this.fruta.precio = 0;
+    this.fruta.id = 0;
   }
 }
--- proyectoentregar/src/app/services/ejercicio004.service.ts
+++ proyectoentregar/src/app/services/ejercicio004.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Fruta } from '../interfaces/fruta';
+import { Fruta } from '../interfaces/Fruta';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,20 @@ export class Ejercicio004Service {
   constructor(private http: HttpClient) { }
   url = 'http://localhost:3000/frutas';
 
-  recuperarFrutas(){
+  recuperarFrutas() : Observable<Fruta[]>{
     return this.http.get<Fruta[]>(`${this.url}?_sort=id&_order=asc`);
     //return this.http.get<Fruta[]>(this.url);
   }
 
-  alta(fruta:any){
+  eliminar(id: number){
+    return this.http.delete<Fruta>(`${this.url}/${id}`);
+  }
 
+  insertar(fruta: Fruta): Observable<Fruta>{
+    return this.http.post<Fruta>(this.url, fruta);
+  }
+
+  modificar(id: number, fruta: Fruta){
+    return this.http.put<Fruta>(`${this.url}/${id}`, fruta);
   }
 }
